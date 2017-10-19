11368b54f839c17c7082a1c4540314033b0c65e3
paige lowe <32681248+paigelowee@users.noreply.github.com>
2017-10-19 12:52:42 +0100

    lab 6

---
+++ assessed work sheet 6/player.py
@@ -0,0 +1,11 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+
+# Start game at the reception
+current_room = rooms["Reception"]
+
+maxmass = 3
+curmass = 0
+
